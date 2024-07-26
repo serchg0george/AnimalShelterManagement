5caa04883fa931a1296a9a13a2d356c3edc7178e
serchg0george <serchg0george@gmail.com>
2024-07-26 13:37:05 +0300

    Initial commit

---
+++ src/main/java/com/animalmanagementsystem/shelter/AnimalShelterManagementApplication.java
@@ -0,0 +1,13 @@
+package com.animalmanagementsystem.shelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalShelterManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/animalmanagementsystem/shelter/AnimalShelterManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animalmanagementsystem.shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
