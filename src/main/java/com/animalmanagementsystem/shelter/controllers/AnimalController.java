3570d83f83ed1cf41a786929c8d503039f89801c
serchg0george <serchg0george@gmail.com>
2024-08-01 14:57:21 +0300

    Implemented CRUD mapping for animal entity

--- src/main/java/com/animalmanagementsystem/shelter/controllers/AnimalController.java
+++ src/main/java/com/animalmanagementsystem/shelter/controllers/AnimalController.java
@@ -2,11 +2,11 @@ package com.animalmanagementsystem.shelter.controllers;
 
 import com.animalmanagementsystem.shelter.dtos.AnimalDto;
 import com.animalmanagementsystem.shelter.services.AnimalService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/animal")
@@ -22,5 +22,27 @@ public class AnimalController {
     public ResponseEntity<String> createAnimal(@RequestBody AnimalDto animalDto) {
         animalService.createAnimal(animalDto);
         return ResponseEntity.ok("Animal created");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AnimalDto>> getAllAnimals() {
+        return new ResponseEntity<>(animalService.getAllAnimals(), HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AnimalDto> getAnimalById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(animalService.getAnimalById(id), HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<String> updateAnimal(@PathVariable("id") Long id, @RequestBody AnimalDto animalDto) {
+        animalService.updateAnimal(animalDto, id);
+        return ResponseEntity.ok("Animal updated");
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteAnimal(@PathVariable("id") Long id) {
+        animalService.deleteAnimal(id);
+        return ResponseEntity.ok("Animal deleted");
     }
 }
