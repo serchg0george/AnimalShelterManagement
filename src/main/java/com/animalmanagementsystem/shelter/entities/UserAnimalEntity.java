f7f7ee497856ecb0f0832b1375e6de1cd18f10ae
serchg0george <serchg0george@gmail.com>
2024-08-05 14:48:47 +0300

    Deleted unused annotations

--- src/main/java/com/animalmanagementsystem/shelter/entities/UserAnimalEntity.java
+++ src/main/java/com/animalmanagementsystem/shelter/entities/UserAnimalEntity.java
@@ -1,7 +1,6 @@
 package com.animalmanagementsystem.shelter.entities;
 
 import com.animalmanagementsystem.shelter.entities.base.BaseEntity;
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
@@ -11,11 +10,11 @@ import jakarta.persistence.Table;
 @Table(name = "t_user_animal")
 public class UserAnimalEntity extends BaseEntity {
 
-    @JsonBackReference
+
     @ManyToOne(cascade = {CascadeType.DETACH})
     UserEntity users;
-    
-    @JsonBackReference
+
+
     @ManyToOne(cascade = {CascadeType.DETACH})
     AnimalEntity animals;
 
