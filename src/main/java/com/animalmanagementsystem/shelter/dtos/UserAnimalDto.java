329c72d5b52af1b3f0f1e2ab95ddd19922ef869b
serchg0george <serchg0george@gmail.com>
2024-08-06 13:07:40 +0300

    Refactored DTO to records

--- src/main/java/com/animalmanagementsystem/shelter/dtos/UserAnimalDto.java
+++ src/main/java/com/animalmanagementsystem/shelter/dtos/UserAnimalDto.java
@@ -1,22 +1,6 @@
 package com.animalmanagementsystem.shelter.dtos;
 
-import com.animalmanagementsystem.shelter.dtos.base.BaseDto;
-
-public class UserAnimalDto extends BaseDto {
-    private final UserDto users;
-    private final AnimalDto animals;
-
-
-    public UserAnimalDto(UserDto users, AnimalDto animals) {
-        this.users = users;
-        this.animals = animals;
-    }
-
-    public UserDto getUsers() {
-        return users;
-    }
-
-    public AnimalDto getAnimals() {
-        return animals;
-    }
+public record UserAnimalDto(Long id,
+                            UserDto users,
+                            AnimalDto animals) {
 }
