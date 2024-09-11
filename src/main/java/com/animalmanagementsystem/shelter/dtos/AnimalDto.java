5e742852730e6201b885a5a5a75780c60cfec628
serchg0george <serchg0george@gmail.com>
2024-09-11 16:30:43 +0300

    Fixed validation

--- src/main/java/com/animalmanagementsystem/shelter/dtos/AnimalDto.java
+++ src/main/java/com/animalmanagementsystem/shelter/dtos/AnimalDto.java
@@ -2,6 +2,7 @@ package com.animalmanagementsystem.shelter.dtos;
 
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 public record AnimalDto(Long id,
                         @NotBlank @Size(max = 150) String name,
                         @NotBlank @Size(max = 50) String species,
-                        @NotBlank @Size(max = 30) Integer age,
+                        @NotNull Integer age,
                         @Nullable CageDto cage,
                         @Nullable List<UserAnimalDto> users,
                         @Nullable HealthDto health) {
