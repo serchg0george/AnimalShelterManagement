216577243f880e3e05210565b7b840b9e5c68a0b
serchg0george <serchg0george@gmail.com>
2024-09-12 15:48:19 +0300

    Removed spring security

--- src/main/java/com/animalmanagementsystem/shelter/authorization/LoginRequest.java
+++
@@ -1,7 +0,0 @@
-package com.animalmanagementsystem.shelter.authorization;
-
-import jakarta.validation.constraints.NotNull;
-
-public record LoginRequest(@NotNull String email,
-                           @NotNull String password) {
-}
